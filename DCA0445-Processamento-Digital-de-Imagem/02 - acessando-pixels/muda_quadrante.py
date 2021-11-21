8c63d4da47770aa4291fab3f18bb1ff9e9ab3cb3
Atyson Jaime <atysonjaime@gmail.com>
2021-11-21 11:42:44 -0300

    :truck: :bento: Altera nomes das pastas e adiciona pasta de img - PDI

--- DCA0445-Processamento-Digital-de-Imagem/02 - acessando-pixels/muda_quadrante.py
+++ DCA0445-Processamento-Digital-de-Imagem/02 - acessando-pixels/muda_quadrante.py
@@ -4,36 +4,30 @@ import cv2
 import sys
 import numpy
 
-cv2.namedWindow('Imagem Normal', cv2.WINDOW_AUTOSIZE)
-cv2.namedWindow('Imagem Quadrantes Trocados', cv2.WINDOW_AUTOSIZE)
-
-img = cv2.imread("biel.png", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("../img/biel.png", cv2.IMREAD_GRAYSCALE)
 if (img is None):
     sys.exit("Não conseguimos abrir a imagem.")
+# Apresenta Imagem Original
+cv2.namedWindow('Imagem Normal', cv2.WINDOW_AUTOSIZE)
 cv2.imshow('Imagem Normal', img)
-cv2.waitKey(0)
 
 altura, largura =img.shape
 
+# Pega os quadrantes da img original
 primeiroQuadrante = img[0:altura//2,0:largura//2]
 segundoQuadrante = img[0:altura//2,largura//2:largura]
 terceiroQuadrante = img[altura//2:altura,0:largura//2]
 quartoQuadrante = img[altura//2:altura,largura//2:largura]
 
-imgTrocada = numpy.zeros((altura, largura), numpy.uint8)  
-cv2.imshow('Imagem Quadrantes Trocados', imgTrocada)
-cv2.waitKey(0)
-    
+# Cria uma nova imagem com os quadrantes da imagem original trocadas.
+imgTrocada = numpy.zeros((altura, largura), numpy.uint8)   
 imgTrocada[0:altura//2,0:largura//2] = quartoQuadrante
-cv2.imshow('Imagem Quadrantes Trocados', imgTrocada)
-cv2.waitKey(0)
 imgTrocada[0:altura//2,largura//2:largura] = terceiroQuadrante
-cv2.imshow('Imagem Quadrantes Trocados', imgTrocada)
-cv2.waitKey(0)
 imgTrocada[altura//2:altura,0:largura//2] = segundoQuadrante
-cv2.imshow('Imagem Quadrantes Trocados', imgTrocada)
-cv2.waitKey(0)
 imgTrocada[altura//2:altura,largura//2:largura] = primeiroQuadrante
+
+# Apresenta Imagem Quadrantes Trocados
+cv2.namedWindow('Imagem Quadrantes Trocados', cv2.WINDOW_AUTOSIZE)
 cv2.imshow('Imagem Quadrantes Trocados', imgTrocada)
 cv2.waitKey(0)
 
