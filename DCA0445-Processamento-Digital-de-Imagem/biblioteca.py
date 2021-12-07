e35ec66e8fb9dc23fe1acf98b47d0f0a03a39d84
Atyson Jaime <atysonjaime@gmail.com>
2021-12-07 17:11:38 -0300

    :sparkles: Adiciona exercício laplgauss em PDI

--- DCA0445-Processamento-Digital-de-Imagem/biblioteca.py
+++ DCA0445-Processamento-Digital-de-Imagem/biblioteca.py
@@ -3,6 +3,22 @@ import cv2
 import sys
 import numpy
 
+def createImage(altura,largura,ehColorida):
+    """[Cria uma imagem de altura e largura definidas pelo usuário]
+
+    Args:
+        altura ([number]): [Altura da Imagem]
+        largura ([number]): [Largura da Imagem]
+        ehColorida ([bool]): [é colorida ?]
+
+    Returns:
+        [list]: [Retorna a imagem criada]
+    """
+    if ehColorida:
+        return np.zeros((altura,largura,3), np.uint8)
+    else:
+        return np.zeros((altura,largura), np.uint8)
+
 cv2.namedWindow('Imagem Exemplo', cv2.WINDOW_AUTOSIZE) ## Usado para criar uma tela para mostrar as imagens
 
 
